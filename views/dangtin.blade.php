a8fec49e2ae5325f257eb23c62953f8dbd259950
ngovudan <67300909+ngovudan@users.noreply.github.com>
2020-06-23 11:12:54 +0700

    Add files via upload

---
+++ views/dangtin.blade.php
@@ -0,0 +1,102 @@
+@extends('layouts._layout')
+@section('content')
+<div class="blog">
+    <div class="section_container">
+        <div class="container">
+            <h1>Đăng Tin Của Bạn</h1>
+            <form action="{{ route('addimage')}}" method="post">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <label>Chọn danh mục đăng tin</label>
+                    <select class="form-control" id="">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label>Chọn tỉnh, thành phố</label>
+                    <select class="form-control" id="">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label>Quận, huyện, thị xã</label>
+                    <select class="form-control" id="">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label>Phường, xã, thị trấn</label>
+                    <select class="form-control" id="">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                    </select>
+                </div>
+                
+                <div class="form-group">
+                    <label for="">Hình ảnh</label>
+                    <div class="custom-file">
+                        <input type="file" class="custom-file-input" name="image">
+                        <label class="custom-file-label"></label>
+                    </div>
+                </div>  
+                <div class="form-group">
+                    <label>Tên</label>
+                    <input type="string" class="form-control" name="name">
+                </div>
+                <div class="form-group">
+                    <label></label>
+                    <input type="string" class="form-control" name="post">
+                </div>
+                
+                <div class="form-group">
+                    <label>Tiêu đề</label>
+                    <input type="text" class="form-control" name="title" placeholder="VD: Quần lửng nữ màu kem size M">
+                </div>
+                <div class="form-group">
+                    <label for="">Mô tả chi tiết</label>
+                <textarea class="form-control _16tLs7dZ58oB3l8shll3x8" placeholder="Viết tiếng Việt có dấu
+  - Loại quần áo
+  - Nam/nữ
+  - Kích cỡ: S,M,L
+  - Nhãn hiệu
+  - Chất liệu, màu sắc" name="detail" rows="12" id="" maxlength="1000"></textarea>
+                </div>!-->
+                <button type="submit" class="btn btn-black">Đăng Bán</button>
+            </form>
+            <!--<form method="post" action="">
+                @csrf
+                <div class="form-group">
+                    <label for="name">Tên</label>
+                    <input type="text" class="form-control" name="origin"/>
+                    
+                </div>
+                <div class="form-group">
+                    <label for="des"></label>
+                    <input type="text" class="form-control" name="destination"/>
+                    @if($errors->has('origin'))
+                    <p> - {{$errors->first('destination')}}</p>
+                    @endif
+                </div>
+                <div class="form-group">
+                    <label for="dur">Duration</label>
+                    <input type="number" class="form-control" name="duration"/>
+                </div>
+                <button type="submit" class="btn btn-block btn-danger">Create data</button>
+            </form>
+            !-->
+        </div>
+    </div>
+    
+</div>
+@endsection
